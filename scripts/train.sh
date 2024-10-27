f2e6cc1c972b246a94b552490f5d6129bfbd9899
Filip Waszak <109302076+washaq00@users.noreply.github.com>
2024-10-27 13:24:26 +0100

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
 for r in {1..25}; do
-	python3 train.py --group=0 --model=orig-ft --load=orig-pre_it200000 --lr=5e-6 --fromIt=$((($r-1)*4000)) --toIt=$(($r*4000));
+	python3 train.py --model=orig-ft --load=orig-pre_it200000 --lr=5e-6 --fromIt=$((($r-1)*4000)) --toIt=$(($r*4000));
 done
